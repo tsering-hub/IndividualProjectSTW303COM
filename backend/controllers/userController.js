5a9467aa15736a46ba4b4d22f0d28c334b3e064f
tsering-hub <ts19sherpa@gmail.com>
2022-11-20 08:45:40 +0545

    Register Route Created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,61 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
+
 // @desc Register New User
 // @route /users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, name, email, password } = req.body;
 
   // Validation
-  if (!username || !email || !password) {
+  if (!username || !name || !email || !password) {
     res.status(400);
     throw new Error("Please include all fields");
   }
-  res.send("register Routes");
+
+  // find if user already exists
+  const userEmailExists = await User.findOne({
+    email,
+  });
+
+  if (userEmailExists) {
+    res.status(400);
+    throw new Error("Email already exists");
+  }
+
+  const usernameExists = await User.findOne({
+    username,
+  });
+
+  if (usernameExists) {
+    res.status(400);
+    throw new Error("Username already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await User.create({
+    username,
+    name,
+    email,
+    password: hashPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
 });
 
 // @desc Login User
