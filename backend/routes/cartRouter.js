8cdd5557e6f07a24e044d3af1d8b7ed00458295b
tsering-hub <ts19sherpa@gmail.com>
2023-01-01 21:06:55 +0545

    auth path changed

--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = new express.Router();
-const auth = require("../auth/auth");
+const auth = require("../middleware/authMiddleware");
 const {
   addtocart,
   getCart,
